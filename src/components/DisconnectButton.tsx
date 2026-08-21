78c8fdf3e6ba66688ab6650723dd63be0e9a9216
mrR <broitman.rom@gmail.com>
2026-08-21 18:52:33 +0300

    update DisconnectButton.tsx

--- src/components/DisconnectButton.tsx
+++ src/components/DisconnectButton.tsx
@@ -1,34 +1,12 @@
-import React from "react";
+import {Button} from "react-native";
+interface Props{onPress:()=>void};
 
-import {
-Button
-} from "react-native";
-
-
-interface Props{
-
-onPress:()=>void;
-
-}
-
-
-export default function DisconnectButton({
-onPress
-}:Props){
-
-
+export default function DisconnectButton({onPress}:Props){
 return (
-
 <Button
-
 title="Disconnect"
-
 color="red"
-
 onPress={onPress}
-
 />
-
 );
-
 }
