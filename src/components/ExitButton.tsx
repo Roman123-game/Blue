afcab1702588633dd8c9ddf804950f11804b1431
mrR <broitman.rom@gmail.com>
2026-08-29 11:47:37 +0300

    update ExitButton.tsx

--- src/components/ExitButton.tsx
+++ src/components/ExitButton.tsx
@@ -39,7 +39,7 @@ export default function ExitButton() {
         style={[styles.xButton, { top: insets.top + 12 }]}
         onPress={() => setVisible(true)}
       >
-        <Text style={styles.xText}>⇩</Text>
+        <Text style={styles.xText}>🡇</Text>
       </TouchableOpacity>
 
       <Modal
